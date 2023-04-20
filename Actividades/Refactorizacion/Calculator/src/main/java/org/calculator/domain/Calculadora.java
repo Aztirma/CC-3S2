94262b7d8a18d326e77cbdb59975c7e72d2970f0
Alejandra Aztirma Zuñiga Chicaña <alejandra.zuniga.c@uni.pe>
2023-04-20 03:04:54 -0500

    Refactorización_Calculator

---
+++ Actividades/Refactorizacion/Calculator/src/main/java/org/calculator/domain/Calculadora.java
@@ -0,0 +1,38 @@
+package org.calculator.domain;
+public class Calculadora {
+    private boolean status;
+    public Calculadora() {
+        this.status = true;
+    }
+    public boolean getStatus() {
+        return status;
+    }
+    public int addition(int a, int b) {
+        return a + b;
+    }
+
+    //Implementación de la función resta
+    public int subtraction(int a, int b) {
+        return a - b;
+    }
+
+    public int division(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("No se puede dividir por cero");
+        } else {
+            return a / b;
+        }
+    }
+
+    //Funciones agregadas
+    public int multiplication(int a, int b) {
+        return a * b;
+    }
+
+    public double squareRoot(double n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("No se puede calcular la raíz cuadrada de un número negativo");
+        }
+        return Math.sqrt(n);
+    }
+}
---
+++ Actividades/Refactorizacion/Calculator/src/test/java/org/calculator/domain/CalculatorTest.java
@@ -0,0 +1,57 @@
+package org.calculator.domain;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+    private static Calculadora calculadora;
+
+    @BeforeAll
+    public static void init(){
+        calculadora = new Calculadora();
+    }
+
+    @Test
+    public void whenCalculatorInitializedThenReturnTrue() {
+        assertTrue(calculadora.getStatus());
+    }
+
+    @Test
+    public void whenAdditionTwoNumberThenReturnCorrectAnswer() {
+        assertEquals( 5, calculadora.addition(2,3));
+    }
+    //Implementacion
+    @Test
+    public void whenSubtractingTwoNumbersThenReturnCorrectAnswer() {
+        assertEquals(2,calculadora.subtraction(5, 3));
+    }
+
+    @Test
+    public void whenDivisionThenReturnCorrectAnswer() {
+        assertEquals(2, calculadora.division(8, 4));
+    }
+    /*
+    @Test
+    public void whenDivisionByZeroThenThrowException() {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+            calculadora.division(5, 0);
+        });
+        assertEquals("No se puede divisor por  cero", exception.getMessage());
+    }*/
+
+    //Ejercicio
+
+    @Test
+    public void whenMultiplyingTwoNumbersThenReturnCorrectAnswer() {
+        assertEquals(15, calculadora.multiplication(5, 3));
+    }
+
+    @Test
+    public void whenCalculatingSquareRootThenReturnCorrectAnswer() {
+        assertEquals(5.0, calculadora.squareRoot(25), 0.0001);
+    }
+
+
+}
