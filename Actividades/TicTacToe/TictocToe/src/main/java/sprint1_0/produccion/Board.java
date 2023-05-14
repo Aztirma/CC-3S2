fa0700e269705aad6dcf3e35338eeaf290162daf
Alejandra Aztirma Zuñiga Chicaña <alejandra.zuniga.c@uni.pe>
2023-05-14 08:53:09 -0500

    Update_Board

--- Actividades/TicTacToe/TictocToe/src/main/java/sprint1_0/produccion/Board.java
+++ Actividades/TicTacToe/TictocToe/src/main/java/sprint1_0/produccion/Board.java
@@ -9,7 +9,10 @@ public class Board {
     }
 
     public int getCell(int row, int column) {
-        return grid[row][column];
+           if(row >= 0 && row < 3 && column >= 0 && column < 3)
+               return grid[row][column];
+           else
+               return -1;
     }
 
     public char getTurn() {
