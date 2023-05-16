c1931041ea97de733394fd9f4a6592b344a4dc47
Alejandra Aztirma Zuñiga Chicaña <alejandra.zuniga.c@uni.pe>
2023-05-16 00:06:43 -0500

    Update ProximoJugadorTest.java

--- Examen Parcial 3S2/Pregunta2/src/test/ProximoJugadorTest.java
+++ Examen Parcial 3S2/Pregunta2/src/test/ProximoJugadorTest.java
@@ -7,4 +7,13 @@ public class ProximoJugadorTest {
         TicTacToe ticTacToe = new TicTacToe();
         assertEquals('X', ticTacToe.proximoJugador());
     }
+    @Test
+    public void O_despuesde_X () {
+        TicTacToe ticTacToe = new TicTacToe();
+
+        assertEquals('X', ticTacToe.proximoJugador()); // Primer turno jugado por X
+        ticTacToe.jugar(1, 1);
+        assertEquals('O', ticTacToe.proximoJugador()); // Segundo turno jugado por O
+    }
+
 }
