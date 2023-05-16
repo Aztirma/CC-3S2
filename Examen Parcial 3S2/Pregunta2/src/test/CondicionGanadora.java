0898241a42d5562aaedcb60cc449e160f1cbf3b3
Alejandra Aztirma Zuñiga Chicaña <alejandra.zuniga.c@uni.pe>
2023-05-16 01:08:08 -0500

    Update CondicionGanadora.java

--- Examen Parcial 3S2/Pregunta2/src/test/CondicionGanadora.java
+++ Examen Parcial 3S2/Pregunta2/src/test/CondicionGanadora.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class CondicionGanadora {
     @Test
@@ -8,4 +9,21 @@ public class CondicionGanadora {
         TicTacToe ticTacToe = new TicTacToe();
         assertFalse(ticTacToe.jugar(1, 1));
     }
+    @Test
+    public void condicionGanadoraHorizontal() {
+        TicTacToe ticTacToe = new TicTacToe();
+
+        // Jugador X ocupa la línea horizontal superior
+        ticTacToe.jugar(0, 0); // X
+        ticTacToe.jugar(1, 0); // O
+        ticTacToe.jugar(0, 1); // X
+        ticTacToe.jugar(1, 1); // O
+        ticTacToe.jugar(0, 2); // X
+
+        // Se espera que haya un ganador en la línea horizontal superior (jugador X)
+        assertTrue(ticTacToe.jugar(1, 2));
+    }
+
+
+
 }
