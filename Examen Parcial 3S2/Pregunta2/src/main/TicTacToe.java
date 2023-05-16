f6e02ecaab44e3843c009ee72f4c0c99a98cf698
Alejandra Aztirma Zuñiga Chicaña <alejandra.zuniga.c@uni.pe>
2023-05-16 02:21:45 -0500

    Refactorizacion

--- Examen Parcial 3S2/Pregunta2/src/main/TicTacToe.java
+++ Examen Parcial 3S2/Pregunta2/src/main/TicTacToe.java
@@ -75,34 +75,57 @@ public class TicTacToe {
     public char proximoJugador() {
         return turn;
     }
-    private boolean hayGanador() {
-        for (int i = 0; i < TOTALROWS; i++) {
-            if (grid[i][0] == grid[i][1] && grid[i][1] == grid[i][2] && grid[i][0] != Cell.EMPTY) {
-                return true; // Hay un ganador en una línea horizontal
-            }
-        }
-        // Verificación de ganancia vertical
-        for (int j = 0; j < TOTALCOLUMNS; j++) {
-            if (grid[0][j] == grid[1][j] && grid[1][j] == grid[2][j] && grid[0][j] != Cell.EMPTY) {
-                return true; // Hay una ganancia vertical
-            }
-        }
-        // Verificar ganancia en diagonales
-        if ((grid[0][0] == grid[1][1] && grid[1][1] == grid[2][2] && grid[0][0] != Cell.EMPTY) ||
-                (grid[0][2] == grid[1][1] && grid[1][1] == grid[2][0] && grid[0][2] != Cell.EMPTY)) {
-            return true; // Hay un ganador en una línea diagonal
-        }
-        return false; // No hay ganador
-    }
 
+    public boolean esGanador(int row, int column) {
+        Cell player = grid[row][column];
+        // Verificar línea horizontal
+        boolean horizontalWin = true;
+        for (int col = 0; col < TOTALCOLUMNS; col++) {
+            if (grid[row][col] != player) {
+                horizontalWin = false;
+                break;
+            }
+        }
+        if (horizontalWin) {
+            return true;
+        }
+        // Verificar línea vertical
+        boolean verticalWin = true;
+        for (int r = 0; r < TOTALROWS; r++) {
+            if (grid[r][column] != player) {
+                verticalWin = false;
+                break;
+            }
+        }
+        if (verticalWin) {
+            return true;
+        }
+        // Verificar diagonales
+        if (row == column || row + column == TOTALROWS - 1) {
+            boolean diagonalWin = true;
+            for (int i = 0; i < TOTALROWS; i++) {
+                if (grid[i][i] != player && grid[i][TOTALROWS - 1 - i] != player) {
+                    diagonalWin = false;
+                    break;
+                }
+            }
+            if (diagonalWin) {
+                return true;
+            }
+        }
+        return false;
+    }
     public boolean hayEmpate() {
         for (int row = 0; row < TOTALROWS; row++) {
-            for (int column = 0; column < TOTALCOLUMNS; column++) {
-                if (grid[row][column] == Cell.EMPTY) {
-                    return false; // Todavía hay casillas vacías, no es un empate
+            for (int col = 0; col < TOTALCOLUMNS; col++) {
+                if (grid[row][col] == Cell.EMPTY || esGanador(row, col)) {
+                    return false; // Todavía hay casillas vacías o hay un ganador, no hay empate
                 }
             }
         }
-        return true; // Todas las casillas están ocupadas, es un empate
+        return true; // Todas las casillas están ocupadas y no hay ganador, hay empate
+    }
+    private boolean hayGanador() {
+        return esGanador(int row, int column);
     }
 }
