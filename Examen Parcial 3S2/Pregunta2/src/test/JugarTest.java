4962455e3fb067795b8a6dffa0628240f3e833df
Alejandra Aztirma Zuñiga Chicaña <alejandra.zuniga.c@uni.pe>
2023-05-15 23:46:36 -0500

    Update JugarTest.java

--- Examen Parcial 3S2/Pregunta2/src/test/JugarTest.java
+++ Examen Parcial 3S2/Pregunta2/src/test/JugarTest.java
@@ -4,10 +4,16 @@ import static org.junit.Assert.*;
 public class JugarTest {
 
     @Test(expected = RuntimeException.class)
-    public void testJugarFueraDeLimites() {
+    public void testJugarFueraDeLimitesX() {
         TicTacToe ticTacToe = new TicTacToe();
         ticTacToe.jugar(5, 2);
     }
+    @Test(expected = RuntimeException.class)
+    public void testJugarFueraDeLimitesY() {
+        TicTacToe ticTacToe = new TicTacToe();
+        ticTacToe.jugar(1, 5);
+    }
+
     @Test(expected = RuntimeException.class)
     public void testJugarLugarOcupado() {
         TicTacToe ticTacToe = new TicTacToe();
